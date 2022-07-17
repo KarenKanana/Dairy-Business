7da77e7467467c61ad49c788090d4b03f26dd7f4
KarenKanana <kananakaren21@gmail.com>
2022-07-17 12:19:22 +0300

    update total production function in js file

---
+++ main.js
@@ -0,0 +1,26 @@
+//function that gives you a report on how much milk is produced per shed and the total amount of milk produced by all sheds
+
+function produceReport() {
+
+    event.preventDefault();
+
+    let data = [];
+
+    data[0] = data["shed-A"] = parseInt(document.getElementById("shed-A").value);
+    data[1] = data["shed-B"] = parseInt(document.getElementById("shed-B").value);
+    data[2] = data["shed-C"] = parseInt(document.getElementById("shed-C").value);
+    data[3] = data["shed-D"] = parseInt(document.getElementById("shed-D").value);
+
+    let sumData = data.shed - A + data.shed - B + data.shed - C + data.shed - D;
+
+    //Output
+    document.getElementById("outputData").innerHTML += "<Br>"
+
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed A is " + data.shed-A + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed B is " + data.shed-B + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed C is " + data.shed-C + " litres per day</p>";
+    document.getElementById("outputData").innerHTML += "<p>Your production in shed D is " + data.shed-D + " litres per day</p>";
+
+    document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + " litres per day</p>";
+}
+console.log(outputData);
